a27ccfe50c6129c0a9a727520036a55ff42e9d01
annaegorova <anna-khabarova2107@yandex.ru>
2023-12-29 00:43:25 +0300

    some refactoring

--- src/main/java/com/anaSense/Main.java
+++ src/main/java/com/anaSense/Main.java
@@ -11,13 +11,14 @@ public class Main {
         Random rand = new Random();
         int i = rand.nextInt(10);
         System.out.println("i = " + i);
-        String message = "";
         if (i == 0) {
-            return message = "Hello QA GURU?";
+            return "Hello QA GURU?";
         } else if (i == 1) {
-            return message = "Hello World?";
+            return "Hello World?";
+        } else if (i>1 && i<5) {
+            return "Hello IntelliJ IDEA?";
         } else {
-            return message = "Hello IntelliJ IDEA?";
+            return "Hello Java?";
         }
     }
 }

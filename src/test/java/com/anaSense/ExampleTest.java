b13f01993e99b316cede867cf5f96c41a659c737
annaegorova <anna-khabarova2107@yandex.ru>
2023-12-29 01:22:09 +0300

    added compare with random digit test

--- src/test/java/com/anaSense/ExampleTest.java
+++ src/test/java/com/anaSense/ExampleTest.java
@@ -1,8 +1,11 @@
+package com.anaSense;
+
 import org.junit.jupiter.api.Test;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -12,6 +15,14 @@ public class ExampleTest {
         LocalDate parsed = LocalDate.parse("28.11.2020", DateTimeFormatter.ofPattern("dd.MM.yyyy"));
 
         assertEquals(DayOfWeek.SATURDAY, parsed.getDayOfWeek());
+    }
 
+    @Test
+    void compareWithRandomDigit() {
+        Random rand = new Random();
+        int i = rand.nextInt(2);
+        System.out.println("i = " + i);
+
+        assertEquals(0,i);
     }
 }

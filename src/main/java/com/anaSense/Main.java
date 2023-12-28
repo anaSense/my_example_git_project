530f25cf3cc37bafd682b5cc5130bcd7d1502253
annaegorova <anna-khabarova2107@yandex.ru>
2023-12-29 00:30:19 +0300

    fix build problems

--- src/main/java/com/anaSense/Main.java
+++ src/main/java/com/anaSense/Main.java
@@ -1,5 +1,7 @@
 package com.anaSense;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println(getMessage());
@@ -8,7 +10,7 @@ public class Main {
     public static String getMessage() {
         Random rand = new Random();
         int i = rand.nextInt(5);
-        System.out.println("i = "+i);
+        System.out.println("i = " + i);
         String message = "";
         if (i == 0) {
             return message = "Hello QA GURU?";
@@ -17,3 +19,4 @@ public class Main {
         } else {
             return message = "Hello IntelliJ IDEA?";
         }
+    }
